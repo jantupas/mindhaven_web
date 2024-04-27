ac51c9f00fef2ac00f9149b52dbb3ac97902aa91
jantupas <janedgartupas@gmail.com>
2024-04-27 15:43:54 +0800

    fifth commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,16 @@
-import firebase from "firebase/app"
-import "firebase/firestore"
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCszx_3ef2rhX3F4LpIRxc9TzZ5oUF3-i8",
-    authDomain: "mindhaven-web.firebaseapp.com",
-    projectId: "mindhaven-web",
-    storageBucket: "mindhaven-web.appspot.com",
-    messagingSenderId: "591919610642",
-    appId: "1:591919610642:web:ea4e88f0549b6b71a911cb"
-}
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export { db }
